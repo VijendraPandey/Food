1b228f6b9969c70b89e7924dc944b5f5f8977219
Vijendra <vijendra.102002@gmail.com>
2024-02-12 01:46:13 +0530

    modified files

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "Namaste Javascript!"
+const root = document.getElementById("root");
+root.appendChild(heading);
+
+const head1 = React.createElement("h1", {}, "Hey there it's React1");
+const head2 = React.createElement("h1", {}, "Hey there it's React2");
+const sect = React.createElement("div",{id: "sect",}, [head1, head2]);
+const roots = ReactDOM.createRoot(document.getElementById("root"));
+roots.render(sect);
