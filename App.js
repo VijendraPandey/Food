ab295b2fad77038e9a3fb9a7e83590d8d0b76563
Vijendra <vijendra.102002@gmail.com>
2024-02-13 00:29:39 +0530

    modified files

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = document.createElement("h1");
-heading.innerHTML = "Namaste Javascript!"
+heading.innerHTML = "Namaste Javascript!";
 const root = document.getElementById("root");
 root.appendChild(heading);
 
-const head1 = React.createElement("h1", {}, "Hey there it's React1");
-const head2 = React.createElement("h1", {}, "Hey there it's React2");
-const sect = React.createElement("div",{id: "sect",}, [head1, head2]);
+const head1 = React.createElement("h1", {}, "Hey there it's Parcel1");
+const head2 = React.createElement("h1", {}, "Hey there it's Parcel2");
+const sect = React.createElement("div", { id: "sect" }, [head1, head2]);
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 roots.render(sect);
